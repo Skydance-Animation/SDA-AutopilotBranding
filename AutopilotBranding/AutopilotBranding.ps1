fc5340110d36ff620f43f21cb25d2efbd95f1990
Javier Negro Dieste <javier.negro@skydance.com>
2024-10-22 20:14:41 +0200

    feat: SDA customization

--- AutopilotBranding/AutopilotBranding.ps1
+++ AutopilotBranding/AutopilotBranding.ps1
@@ -38,143 +38,143 @@ Log "Loading configuration: $($installFolder)Config.xml"
 [Xml]$config = Get-Content "$($installFolder)Config.xml"
 
 # STEP 1: Apply custom start menu layout
-$ci = Get-ComputerInfo
-if ($ci.OsBuildNumber -le 22000) {
-	Log "Importing layout: $($installFolder)Layout.xml"
-	Copy-Item "$($installFolder)Layout.xml" "C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\LayoutModification.xml" -Force
-} else {
-	Log "Importing layout: $($installFolder)Start2.bin"
-	MkDir -Path "C:\Users\Default\AppData\Local\Packages\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\LocalState" -Force -ErrorAction SilentlyContinue | Out-Null
-	Copy-Item "$($installFolder)Start2.bin" "C:\Users\Default\AppData\Local\Packages\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\LocalState\Start2.bin" -Force
-}
+# $ci = Get-ComputerInfo
+# if ($ci.OsBuildNumber -le 22000) {
+# 	Log "Importing layout: $($installFolder)Layout.xml"
+# 	Copy-Item "$($installFolder)Layout.xml" "C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\LayoutModification.xml" -Force
+# } else {
+# 	Log "Importing layout: $($installFolder)Start2.bin"
+# 	MkDir -Path "C:\Users\Default\AppData\Local\Packages\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\LocalState" -Force -ErrorAction SilentlyContinue | Out-Null
+# 	Copy-Item "$($installFolder)Start2.bin" "C:\Users\Default\AppData\Local\Packages\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\LocalState\Start2.bin" -Force
+# }
 
 # STEP 2: Configure background
 reg.exe load HKLM\TempUser "C:\Users\Default\NTUSER.DAT" | Out-Host
 
 Log "Setting up Autopilot theme"
 Mkdir "C:\Windows\Resources\OEM Themes" -Force | Out-Null
-Copy-Item "$installFolder\Autopilot.theme" "C:\Windows\Resources\OEM Themes\Autopilot.theme" -Force
+Copy-Item "$installFolder\Skydance.theme" "C:\Windows\Resources\OEM Themes\Skydance.theme" -Force
 Mkdir "C:\Windows\web\wallpaper\Autopilot" -Force | Out-Null
-Copy-Item "$installFolder\Autopilot.jpg" "C:\Windows\web\wallpaper\Autopilot\Autopilot.jpg" -Force
+Copy-Item "$installFolder\Skydance.jpg" "C:\Windows\web\wallpaper\Autopilot\Skydance.jpg" -Force
 Log "Setting Autopilot theme as the new user default"
-reg.exe add "HKLM\TempUser\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes" /v InstallTheme /t REG_EXPAND_SZ /d "%SystemRoot%\resources\OEM Themes\Autopilot.theme" /f | Out-Host
+reg.exe add "HKLM\TempUser\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes" /v InstallTheme /t REG_EXPAND_SZ /d "%SystemRoot%\resources\OEM Themes\Skydance.theme" /f | Out-Host
 
 # STEP 2A: Stop Start menu from opening on first logon
-reg.exe add "HKLM\TempUser\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v StartShownOnUpgrade /t REG_DWORD /d 1 /f | Out-Host
+# reg.exe add "HKLM\TempUser\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v StartShownOnUpgrade /t REG_DWORD /d 1 /f | Out-Host
 
 # STEP 2B: Hide "Learn more about this picture" from the desktop
-reg.exe add "HKLM\TempUser\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{2cc5ca98-6485-489a-920e-b3e88a6ccce3}" /t REG_DWORD /d 1 /f | Out-Host
+# reg.exe add "HKLM\TempUser\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{2cc5ca98-6485-489a-920e-b3e88a6ccce3}" /t REG_DWORD /d 1 /f | Out-Host
 
 reg.exe unload HKLM\TempUser | Out-Host
 
 # STEP 3: Set time zone (if specified)
-if ($config.Config.TimeZone) {
-	Log "Setting time zone: $($config.Config.TimeZone)"
-	Set-Timezone -Id $config.Config.TimeZone
-}
-else {
-	# Enable location services so the time zone will be set automatically (even when skipping the privacy page in OOBE) when an administrator signs in
-	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\location" -Name "Value" -Type "String" -Value "Allow" -Force
-	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Sensor\Overrides\{BFA794E4-F964-4FDB-90F6-51056BFE4B44}" -Name "SensorPermissionState" -Type "DWord" -Value 1 -Force
-	Start-Service -Name "lfsvc" -ErrorAction SilentlyContinue
-}
+# if ($config.Config.TimeZone) {
+# 	Log "Setting time zone: $($config.Config.TimeZone)"
+# 	Set-Timezone -Id $config.Config.TimeZone
+# }
+# else {
+# 	# Enable location services so the time zone will be set automatically (even when skipping the privacy page in OOBE) when an administrator signs in
+# 	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\location" -Name "Value" -Type "String" -Value "Allow" -Force
+# 	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Sensor\Overrides\{BFA794E4-F964-4FDB-90F6-51056BFE4B44}" -Name "SensorPermissionState" -Type "DWord" -Value 1 -Force
+# 	Start-Service -Name "lfsvc" -ErrorAction SilentlyContinue
+# }
 
 # STEP 4: Remove specified provisioned apps if they exist
-Log "Removing specified in-box provisioned apps"
-$apps = Get-AppxProvisionedPackage -online
-$config.Config.RemoveApps.App | % {
-	$current = $_
-	$apps | ? {$_.DisplayName -eq $current} | % {
-		try {
-			Log "Removing provisioned app: $current"
-			$_ | Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue | Out-Null
-		} catch { }
-	}
-}
+# Log "Removing specified in-box provisioned apps"
+# $apps = Get-AppxProvisionedPackage -online
+# $config.Config.RemoveApps.App | % {
+# 	$current = $_
+# 	$apps | ? {$_.DisplayName -eq $current} | % {
+# 		try {
+# 			Log "Removing provisioned app: $current"
+# 			$_ | Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue | Out-Null
+# 		} catch { }
+# 	}
+# }
 
 # STEP 5: Install OneDrive per machine
-if ($config.Config.OneDriveSetup) {
-	Log "Downloading OneDriveSetup"
-	$dest = "$($env:TEMP)\OneDriveSetup.exe"
-	$client = new-object System.Net.WebClient
-	$client.DownloadFile($config.Config.OneDriveSetup, $dest)
-	Log "Installing: $dest"
-	$proc = Start-Process $dest -ArgumentList "/allusers" -WindowStyle Hidden -PassThru
-	$proc.WaitForExit()
-	Log "OneDriveSetup exit code: $($proc.ExitCode)"
-}
+# if ($config.Config.OneDriveSetup) {
+# 	Log "Downloading OneDriveSetup"
+# 	$dest = "$($env:TEMP)\OneDriveSetup.exe"
+# 	$client = new-object System.Net.WebClient
+# 	$client.DownloadFile($config.Config.OneDriveSetup, $dest)
+# 	Log "Installing: $dest"
+# 	$proc = Start-Process $dest -ArgumentList "/allusers" -WindowStyle Hidden -PassThru
+# 	$proc.WaitForExit()
+# 	Log "OneDriveSetup exit code: $($proc.ExitCode)"
+# }
 
 # STEP 6: Don't let Edge create a desktop shortcut (roams to OneDrive, creates mess)
 Log "Turning off (old) Edge desktop shortcut"
 reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /v DisableEdgeDesktopShortcutCreation /t REG_DWORD /d 1 /f /reg:64 | Out-Host
 
 # STEP 7: Add language packs
-Get-ChildItem "$($installFolder)LPs" -Filter *.cab | % {
-	Log "Adding language pack: $($_.FullName)"
-	Add-WindowsPackage -Online -NoRestart -PackagePath $_.FullName
-}
+# Get-ChildItem "$($installFolder)LPs" -Filter *.cab | % {
+# 	Log "Adding language pack: $($_.FullName)"
+# 	Add-WindowsPackage -Online -NoRestart -PackagePath $_.FullName
+# }
 
 # STEP 8: Change language
-if ($config.Config.Language) {
-	Log "Configuring language using: $($config.Config.Language)"
-	& $env:SystemRoot\System32\control.exe "intl.cpl,,/f:`"$($installFolder)$($config.Config.Language)`""
-}
+# if ($config.Config.Language) {
+# 	Log "Configuring language using: $($config.Config.Language)"
+# 	& $env:SystemRoot\System32\control.exe "intl.cpl,,/f:`"$($installFolder)$($config.Config.Language)`""
+# }
 
 # STEP 9: Add features on demand
-$currentWU = (Get-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU" -ErrorAction Ignore).UseWuServer
-if ($currentWU -eq 1)
-{
-	Log "Turning off WSUS"
-	Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU"  -Name "UseWuServer" -Value 0
-	Restart-Service wuauserv
-}
-if ($config.Config.AddFeatures.Feature.Count -gt 0)
-{
-	$config.Config.AddFeatures.Feature | % {
-		Log "Adding Windows feature: $_"
-		Add-WindowsCapability -Online -Name $_ -ErrorAction SilentlyContinue | Out-Null
-	}
-}
-if ($currentWU -eq 1)
-{
-	Log "Turning on WSUS"
-	Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU"  -Name "UseWuServer" -Value 1
-	Restart-Service wuauserv
-}
+# $currentWU = (Get-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU" -ErrorAction Ignore).UseWuServer
+# if ($currentWU -eq 1)
+# {
+# 	Log "Turning off WSUS"
+# 	Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU"  -Name "UseWuServer" -Value 0
+# 	Restart-Service wuauserv
+# }
+# if ($config.Config.AddFeatures.Feature.Count -gt 0)
+# {
+# 	$config.Config.AddFeatures.Feature | % {
+# 		Log "Adding Windows feature: $_"
+# 		Add-WindowsCapability -Online -Name $_ -ErrorAction SilentlyContinue | Out-Null
+# 	}
+# }
+# if ($currentWU -eq 1)
+# {
+# 	Log "Turning on WSUS"
+# 	Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU"  -Name "UseWuServer" -Value 1
+# 	Restart-Service wuauserv
+# }
 
 # STEP 10: Customize default apps
-if ($config.Config.DefaultApps) {
-	Log "Setting default apps: $($config.Config.DefaultApps)"
-	& Dism.exe /Online /Import-DefaultAppAssociations:`"$($installFolder)$($config.Config.DefaultApps)`"
-}
+# if ($config.Config.DefaultApps) {
+# 	Log "Setting default apps: $($config.Config.DefaultApps)"
+# 	& Dism.exe /Online /Import-DefaultAppAssociations:`"$($installFolder)$($config.Config.DefaultApps)`"
+# }
 
 # STEP 11: Set registered user and organization
-Log "Configuring registered user information"
-reg.exe add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v RegisteredOwner /t REG_SZ /d "$($config.Config.RegisteredOwner)" /f /reg:64 | Out-Host
-reg.exe add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v RegisteredOrganization /t REG_SZ /d "$($config.Config.RegisteredOrganization)" /f /reg:64 | Out-Host
+# Log "Configuring registered user information"
+# reg.exe add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v RegisteredOwner /t REG_SZ /d "$($config.Config.RegisteredOwner)" /f /reg:64 | Out-Host
+# reg.exe add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v RegisteredOrganization /t REG_SZ /d "$($config.Config.RegisteredOrganization)" /f /reg:64 | Out-Host
 
 # STEP 12: Configure OEM branding info
-if ($config.Config.OEMInfo)
-{
-	Log "Configuring OEM branding info"
+# if ($config.Config.OEMInfo)
+# {
+# 	Log "Configuring OEM branding info"
 
-	reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v Manufacturer /t REG_SZ /d "$($config.Config.OEMInfo.Manufacturer)" /f /reg:64 | Out-Host
-	reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v Model /t REG_SZ /d "$($config.Config.OEMInfo.Model)" /f /reg:64 | Out-Host
-	reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v SupportPhone /t REG_SZ /d "$($config.Config.OEMInfo.SupportPhone)" /f /reg:64 | Out-Host
-	reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v SupportHours /t REG_SZ /d "$($config.Config.OEMInfo.SupportHours)" /f /reg:64 | Out-Host
-	reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v SupportURL /t REG_SZ /d "$($config.Config.OEMInfo.SupportURL)" /f /reg:64 | Out-Host
-	Copy-Item "$installFolder\$($config.Config.OEMInfo.Logo)" "C:\Windows\$($config.Config.OEMInfo.Logo)" -Force
-	reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v Logo /t REG_SZ /d "C:\Windows\$($config.Config.OEMInfo.Logo)" /f /reg:64 | Out-Host
-}
+# 	reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v Manufacturer /t REG_SZ /d "$($config.Config.OEMInfo.Manufacturer)" /f /reg:64 | Out-Host
+# 	reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v Model /t REG_SZ /d "$($config.Config.OEMInfo.Model)" /f /reg:64 | Out-Host
+# 	reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v SupportPhone /t REG_SZ /d "$($config.Config.OEMInfo.SupportPhone)" /f /reg:64 | Out-Host
+# 	reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v SupportHours /t REG_SZ /d "$($config.Config.OEMInfo.SupportHours)" /f /reg:64 | Out-Host
+# 	reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v SupportURL /t REG_SZ /d "$($config.Config.OEMInfo.SupportURL)" /f /reg:64 | Out-Host
+# 	Copy-Item "$installFolder\$($config.Config.OEMInfo.Logo)" "C:\Windows\$($config.Config.OEMInfo.Logo)" -Force
+# 	reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v Logo /t REG_SZ /d "C:\Windows\$($config.Config.OEMInfo.Logo)" /f /reg:64 | Out-Host
+# }
 
 # STEP 13: Enable UE-V
-Log "Enabling UE-V"
-Enable-UEV
-Set-UevConfiguration -Computer -SettingsStoragePath "%OneDriveCommercial%\UEV" -SyncMethod External -DisableWaitForSyncOnLogon
-Get-ChildItem "$($installFolder)UEV" -Filter *.xml | % {
-	Log "Registering template: $($_.FullName)"
-	Register-UevTemplate -Path $_.FullName
-}
+# Log "Enabling UE-V"
+# Enable-UEV
+# Set-UevConfiguration -Computer -SettingsStoragePath "%OneDriveCommercial%\UEV" -SyncMethod External -DisableWaitForSyncOnLogon
+# Get-ChildItem "$($installFolder)UEV" -Filter *.xml | % {
+# 	Log "Registering template: $($_.FullName)"
+# 	Register-UevTemplate -Path $_.FullName
+# }
 
 # STEP 14: Disable network location fly-out
 Log "Turning off network location fly-out"
